06d44be0934ce14740ac79968bb134bbdca6d092
GuangXuan <tlfggx@gmail.com>
2018-07-23 20:24:27 +0800

    导航栏 :ok_hand:

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,51 +1,37 @@
-const weatherMap = {
-  'sunny': '晴',
-  'cloudy': '多云',
-  'overcast': '阴',
-  'lightrain': '小雨',
-  'heavyrain': '大雨',
-  'snow': '雪'
-}
-
-const weatherColorMap = {
-  'sunny': '#cbeefd',
-  'cloudy': '#deeef6',
-  'overcast': '#c6ced2',
-  'lightrain': '#bdd5e1',
-  'heavyrain': '#c5ccd0',
-  'snow': '#aae1fc'
-}
-
+let selColor = "#ffffff"
+let unSelColor = "#ced3dc"
 
 Page({
   data: {
-    nowTemp: '9°',
-    nowWeather: '晴',
-    nowWeatherBackground: '/images/sunny-bg.png',
+    newsTypeList: [
+      {"text": "国内", "id": "gn", "color":selColor},
+      {"text": "国际", "id": "gj", "color": unSelColor},
+      { "text": "财经", "id": "cj", "color": unSelColor},
+      { "text": "娱乐", "id": "yl", "color": unSelColor},
+      { "text": "军事", "id": "js", "color": unSelColor},
+      { "text": "体育", "id": "ty", "color": unSelColor},
+      { "text": "其他", "id": "other", "color": unSelColor}
+    ]
   },
-  onLoad() {
-    console.log("Hello World!")
-    wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now',
-      data: {
-        city: '广州市'
-      },
-      success: res=> {
-        console.log(res)
-        let result = res.data.result
-        let temp = result.now.temp
-        let weather = result.now.weather
-        console.log(temp, weather)
-        this.setData({
-          nowTemp: temp + '°',
-          nowWeather: weatherMap[weather],
-          nowWeatherBackground: '/images/' + weather + '-bg.png',
-        })
-        wx.setNavigationBarColor({
-          frontColor: '#000000',
-          backgroundColor: weatherColorMap[weather],
-        })
+  onLoad(){
+
+  },
+  //点击新闻类别跳转并获取新闻列表
+  selNewsType(e) {
+    if (this.data.selectedNewsType == e.currentTarget.dataset.id) {
+      return;
+    }
+    this.data.newsTypeList.forEach(function (value) {
+      if (value.id == e.currentTarget.dataset.id) {
+        value.color = selColor;
+      } else {
+        value.color = unSelColor;
       }
+    });
+    this.setData({
+      newsTypeList: this.data.newsTypeList,
+      selectedNewsType: e.currentTarget.dataset.id
     })
-  }
+    this.getNews(); //获取新闻列表
+  },
 })
