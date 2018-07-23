14d7609b5a99249dd3b738c521e2add5ab57e9c3
GuangXuan <tlfggx@gmail.com>
2018-07-23 12:30:02 +0800

    导航栏 :ok_hand:

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,51 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+
+Page({
+  data: {
+    nowTemp: '9°',
+    nowWeather: '晴',
+    nowWeatherBackground: '/images/sunny-bg.png',
+  },
+  onLoad() {
+    console.log("Hello World!")
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res=> {
+        console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png',
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      }
+    })
+  }
+})
