513d8cfcb589ec613ab12efa00f94d26e70567b6
GuangXuan <tlfggx@gmail.com>
2018-07-25 11:16:08 +0800

    加入触摸事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,10 @@ let selColor = "#ffffff"
 let unSelColor = "#ced3dc"
 let selLine = "1px solid #fff"
 let unSelLine = ""
+var touchDirection = 0;
+var touchDot = 0;//触摸时的原点
+var time = 0; // 时间记录，用于滑动时且时间小于1s则执行左右滑动
+var interval = ""; //计时器
 
 Page({
   data: {
@@ -70,6 +74,14 @@ Page({
           secendNewsList: secendList
         })
       },
+      fail: ()=>{
+        wx.hideLoading();
+        wx.showToast({
+          title: '网络错误,请重新刷新',
+          icon: 'none',
+          duration: 2000
+        })
+      },
       complete: ()=>{
         callback && callback();
       }
@@ -92,5 +104,51 @@ Page({
       };
       result[i].source = result[i].source + "  "+ result[i].date.slice(0,10) +"  "+ result[i].date.slice(11,16)
     }
+  },
+  // 触摸滑动事件
+  touchStart: function (e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间  
+    interval = setInterval(function () {
+      time++;
+    }, 100);
+  },
+  touchMove: function (e) {
+    var touchMove = e.touches[0].pageX;
+    // 向左滑动  
+    if (touchMove - touchDot <= -100 && time < 100) {
+      touchDirection = 1;
+    }
+    // 向右滑动
+    if (touchMove - touchDot >= 100 && time < 100) {
+      touchDirection = -1;
+    }
+  },
+  touchEnd: function (e) {
+    this.changeNewsType(touchDirection);
+    clearInterval(interval); 
+    time = 0;
+    touchDirection = 0;
+  },
+  changeNewsType(num) {
+    for (var i = 0; i < this.data.newsTypeList.length; i++) {
+      if (this.data.newsTypeList[i].tp == this.data.selectedNewsType) {
+        if ((num == -1 && i == 0) || (num == 1 && i == this.data.newsTypeList.length - 1)) {
+          return;
+        } else {
+          this.data.newsTypeList[i].color = unSelColor;
+          this.data.newsTypeList[i].line = unSelLine;
+          this.data.newsTypeList[i + num].color = selColor;
+          this.data.newsTypeList[i + num].line = selLine;
+          this.data.selectedNewsType = this.data.newsTypeList[i + num].tp;
+          break;
+        }
+      }
+    }
+    this.setData({
+      newsTypeList: this.data.newsTypeList,
+      selectedNewsType: this.data.selectedNewsType
+    });
+    this.getNewsList();
   }
 })
